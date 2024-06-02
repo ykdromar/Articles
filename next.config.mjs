e1e7c9ab7e8f95ea8e814bbe3c9abd7065289316
Yash <ykdromarpro@gmail.com>
2024-06-02 21:20:39 +0530

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // This applies to all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
